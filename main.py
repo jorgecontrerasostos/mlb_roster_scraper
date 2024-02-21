591b48af623294ca5c5521edb0ba5a3d136ee54e
jorgecontrerasostos <jorge.contreras@justia.com>
2024-02-21 17:30:06 -0600

    output players and team

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from helpers import fetch_team_urls, get_team_players
+from pprint import pprint
+
+
 def main():
-    team_urls = []
+    team_players_dict = {}
     teams = [
         'redsox',
         'yankees',
@@ -34,11 +37,13 @@ def main():
         'giants',
     ]
     for team in teams:
-        team_urls.append(f'https://www.mlb.com/{team}/roster/40-man')
+        team_url = f'https://www.mlb.com/{team}/roster/40-man'
+        team_content = fetch_team_urls([team_url])
+        team_players = get_team_players(team_content)
+        team_players_dict[team] = team_players
     
-    results = fetch_team_urls(team_urls)
-    team_players = get_team_players(results)
-    print(team_players)
+    for team, players in team_players_dict.items():
+        pprint(f"{team}: {players}")
 
 if __name__ == '__main__':
     main()
